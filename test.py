0dd0197a64b1e8bd7eb5bd0eb3e3be78b78d2903
raptor271828 <isagyroscope@gmail.com>
2025-05-05 22:39:53 -0400

    test: ln_well

--- test.py
+++ test.py
@@ -3,21 +3,25 @@ import Schro_Time_Dependant_2D as schro
 import numpy as np
 out_path = "./test/"
 
-domain = schro.rectangular_1_particle_domain((20,20),5)
+simulating= (input("are_you_simulating?: (y/n): ")=="y")
 
-V = np.log((domain.X**2 + domain.Y**2)**0.5)*100
+
+domain = schro.rectangular_1_particle_domain((20,20),10)
+
+V = np.log((domain.X**2 + domain.Y**2)**1)*0.25
 V[V==-inf] = V[V != -inf].min()
-V = 0
-initial_psi = schro.normalized_2d_gaussian(domain.X,domain.Y,0.5, dX=4) * np.exp(1j*domain.Y*7)
 
-# t, psi = domain.time_evolution(initial_psi, np.linspace(0,150,150), V)
+initial_psi = schro.normalized_2d_gaussian(domain.X,domain.Y,0.5, dX=4) * np.exp(1j*domain.Y*3.5)
 
-# domain_params = np.array([domain.width, domain.height, domain. resolution])
+if simulating:
+    t, psi = domain.time_evolution(initial_psi, np.linspace(0,1500,1500), V)
 
-# np.save(out_path+"_V.npy",V)
-# np.save(out_path+"_domain_whr.npy",domain_params)
-# np.save(out_path+"_psi.npy", psi)
-# np.save(out_path+"_t.npy", t)
+    domain_params = np.array([domain.width, domain.height, domain. resolution])
+
+    np.save(out_path+"_V.npy",V)
+    np.save(out_path+"_domain_whr.npy",domain_params)
+    np.save(out_path+"_psi.npy", psi)
+    np.save(out_path+"_t.npy", t)
 
 psi = np.load(out_path+"_psi.npy")
 t = np.load(out_path+"_t.npy")
