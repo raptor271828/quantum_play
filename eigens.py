db8a0c3e9354fbd86724fd5782987f00340f0d4d
raptor271828 <isagyroscope@gmail.com>
2025-05-07 13:10:44 -0400

    some aspects of eigens finder, but weiirrddd errors. switching to real space now

--- eigens.py
+++ eigens.py
@@ -3,16 +3,17 @@ import scipy.linalg as sp
 #find eignenvectors via variational method (eignevalues must be bounded from below)
 
 class eigenvector_finder:
-    def __init__(self, O, H, num_basis, num_eigens) -> None:
+    def __init__(self, O, H,num_basis, num_eigens, K=None) -> None:
         self.O = O
         self.H = H
-
+        self.K = K
         #initial guess
         self.W_initial=np.random.rand(num_basis, num_eigens)+1j*np.random.rand(num_basis, num_eigens)
+        self.W_initial=self.W_initial@np.linalg.inv(sp.sqrtm(self.U(self.W_initial))) #orthonormalize
 
-    def find_eignens(self, Nit_lm, Nit_cg, conv_crit_cg):
-        W, _ = self.lm(self.W_initial,Nit_lm)
-        W, _ = self.lm(W,Nit_cg, conv_criterion=conv_crit_cg)
+    def find_eignens(self, Nit_lm, Nit_cg, conv_crit_cg, pc=False):
+        W, _ = self.lm(self.W_initial,Nit_lm, pc=pc)
+        #W, _ = self.cg(W,Nit_cg, conv_criterion=conv_crit_cg)
 
         eigenvectors, eigenvalues = self.getpsi(W)
 
@@ -20,13 +21,14 @@ class eigenvector_finder:
 
 
     def U(self, W):
-        return W.T.conj(self.O(W))
+        return W.T.conj()@self.O(W)
 
     def dot(self, W1,W2):
         return (W1.conj()*W2).sum()
 
     def getexpect(self, W):
-        return self.dot(W,self.H(W))
+
+        return self.dot(W,self.H(W@np.linalg.inv(sp.sqrtm(self.U(W)))))
 
     def getgrad(self, W):
         U_inv = np.linalg.inv(self.U(W))
@@ -61,9 +63,9 @@ class eigenvector_finder:
                 beta = self.dot((g - g_old),g)/(self.dot(g_old, g_old))
                 d=-g + beta * d_old
 
-            # if i != 0 :
-            #     print(f"linmin test{self.dot(g,d_old)*(self.dot(g,g) * self.dot(d_old,d_old))**-0.5}")
-            #     print(f" cg test{self.dot(g,g_old)*(self.dot(g,g) * self.dot(g_old,g_old))**-0.5}")
+            if i != 0 :
+                print(f"linmin test{self.dot(g,d_old)*(self.dot(g,g) * self.dot(d_old,d_old))**-0.5}")
+                print(f" cg test{self.dot(g,g_old)*(self.dot(g,g) * self.dot(g_old,g_old))**-0.5}")
 
             g_t = self.getgrad(W_old+alpha_trial*d)
             best_alpha = alpha_trial * self.dot(g,d)/(self.dot((g-g_t),d))
@@ -73,14 +75,16 @@ class eigenvector_finder:
             #printProgressBar(i,Nit+1,suffix=f"Complete E={getE(W)}")
             Elist[i+1]=self.getexpect(W)
 
-            if (Elist[i+1]-Elist[i])/Elist[i+1] < conv_criterion:
+            print(f"\r cg: iter {i/Nit:02f}, conv: {(Elist[i+1]-Elist[i])}, expectation:{Elist[i+1]}", end="")
+
+
+            if (-(Elist[i+1]-Elist[i])< conv_criterion) & ((Elist[i+1]-Elist[i]) < 0):
                 Elist=Elist[:i+2]
                 break
-                print(f"\r cg: iter {i/Nit:02f}, conv: {(Elist[i+1]-Elist[i])/Elist[i+1]}", end="")
         return W, Elist
 
 
-    def lm(self, W,Nit, conv_criterion=0):
+    def lm(self, W,Nit, conv_criterion=0, pc=False):
         Elist=np.zeros(Nit+1,dtype=complex)
         Elist[0]=self.getexpect(W)
         alpha_trial=0.00003
@@ -90,10 +94,13 @@ class eigenvector_finder:
             d_old = d
 
             g = self.getgrad(W_old)
-            d=-g
+            if pc:
+                d=-self.K(g) #type: ignore
+            else:
+                d=-g
 
-            # if i != 0 :
-            #     print(f"linmin test{self.dot(g,d_old)/(self.dot(g,g) * self.dot(d_old,d_old))}")
+            if i != 0 :
+                print(f"linmin test{self.dot(g,d_old)/(self.dot(g,g) * self.dot(d_old,d_old))}")
 
             g_t = self.getgrad(W_old+alpha_trial*d)
             best_alpha = alpha_trial * self.dot(g,d)/(self.dot((g-g_t),d))
@@ -103,9 +110,10 @@ class eigenvector_finder:
             #printProgressBar(i,Nit+1,suffix=f"Complete E={getE(W)}")
             Elist[i+1]=self.getexpect(W)
 
-            if (Elist[i+1]-Elist[i])/Elist[i+1] < conv_criterion:
+            print(f"\r lm: iter {i/Nit:02f}, conv: {(Elist[i+1]-Elist[i])}, expectation:{Elist[i+1]}", end="")
+
+            if (-(Elist[i+1]-Elist[i]) < conv_criterion) & ((Elist[i+1]-Elist[i])< 0):
                 Elist=Elist[:i+2]
                 break
-            print(f"\r lm: iter {i/Nit:02f}, conv: {(Elist[i+1]-Elist[i])/Elist[i+1]}", end="")
             #printProgressBar(i+1,Nit,suffix=f"Complete")
         return W, Elist
---
+++ eigens_test.py
@@ -0,0 +1,57 @@
+import Schro_Time_Dependant_2D as schro
+import eigens
+import numpy as np
+out_path = "./eigens/"
+in_path = "./test/"
+file_prefix=input("file name: ")
+minimizing= (input("are_you_minizing? (y/n): ")=="y")
+
+domain_params = np.load(in_path+file_prefix+"_domain_whr.npy")
+V=np.load(in_path+file_prefix+"_V.npy")
+print(V)
+psi = np.load(in_path+file_prefix+"_psi.npy")
+#t = np.load(out_path+file_prefix+"_t.npy")
+domain = schro.rectangular_1_particle_domain((domain_params[0],domain_params[1]),domain_params[2])
+
+
+
+
+if minimizing:
+
+    #plane wave basis
+    def O_for_minimizer(W_unraveled):
+        return W_unraveled * domain.width * domain.height
+
+    def K_for_minimizer(W_unraveled):
+        return W_unraveled * (domain.G_sqr.reshape((domain.num_X*domain.num_Y,1)) + 1)**-1
+
+    def H_for_minimizer(W_unraveled, m=1, hbar=1):
+        T = hbar**2 * (domain.G_sqr.reshape((domain.num_X*domain.num_Y,1)) * W_unraveled)/(2*m)
+        U = np.fft.fft2(
+            V[:,:,np.newaxis]*np.fft.ifft2(
+                W_unraveled.reshape(domain.num_X,domain.num_Y, -1)
+            )
+        ).reshape(domain.num_X*domain.num_Y,-1)
+
+        return O_for_minimizer(T+U)
+
+    finder = eigens.eigenvector_finder(O_for_minimizer,H_for_minimizer,domain.num_X*domain.num_Y ,5, K=K_for_minimizer)
+    eigenvectors, eigenvalues = finder.find_eignens(40,0,0, pc=False)
+
+    np.save(out_path+file_prefix+"_eigenvectors.npy", eigenvectors)
+    np.save(out_path+file_prefix+"_eigenvalues.npy", eigenvalues)
+    print(eigenvalues)
+
+
+else:
+    eigenvectors = np.load(out_path+file_prefix+"_eigenvectors.npy")
+    eigenvalues = np.load(out_path+file_prefix+"_eigenvalues.npy")
+
+eigenvectors_raveled = eigenvectors.reshape((domain.num_X, domain.num_Y, -1))
+
+    # eignenvectors_raveled_list = []
+    # for i in range(eigenvectors_raveled.shape[-1]):
+    #     eignenvectors_raveled_list.append(eigenvectors_raveled[:,:,i, np.newaxis]) #extra unused dimension for
+    #
+print(eigenvectors_raveled.shape)
+schro.plot_and_save_psi_vs_t((domain.real_space(eigenvectors_raveled),), np.ones(eigenvectors.shape[-1]), "./eigens/vector")
